ac60f54758cb1b33728db20f1dc590ea4b0d41eb
ConnorDW-SA <114667779+ConnorDW-SA@users.noreply.github.com>
2023-04-11 23:29:36 +0200

    .

--- src/api/models/games.js
+++ src/api/models/games.js
@@ -11,27 +11,37 @@ const GameSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true
-    }
-    // boardState: {
-    //   type: Array,
-    //   required: true,
-    // },
-    // currentPlayer: {
-    //   type: String,
-    //   enum: ['white', 'black'],
-    //   required: true,
-    // },
-    // status: {
-    //   type: String,
-    //   enum: ['in-progress', 'finished'],
-    //   required: true,
-    //   default: 'in-progress',
-    // },
-    // winner: {
-    //   type: String,
-    //   enum: ['white', 'black', 'draw', null],
-    //   default: null,
-    // },
+    },
+    boardState: {
+      type: Array,
+      required: true
+    },
+    currentPlayer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+    status: {
+      type: String,
+      enum: ["in-progress", "finished"],
+      required: true,
+      default: "in-progress"
+    },
+    winner: {
+      type: String,
+      enum: ["white", "black", "draw", null],
+      default: null
+    },
+    moveHistory: [
+      {
+        from: { type: String, required: true },
+        to: { type: String, required: true },
+        piece: { type: String, required: true },
+        color: { type: String, enum: ["white", "black"], required: true },
+        capturedPiece: { type: String },
+        promotion: { type: String }
+      }
+    ]
   },
   { timestamps: true }
 );
