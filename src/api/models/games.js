e3890c32a1e80a27a145f1407189ade7478d3430
ConnorDW-SA <114667779+ConnorDW-SA@users.noreply.github.com>
2023-04-17 20:07:06 +0200

    .

--- src/api/models/games.js
+++ src/api/models/games.js
@@ -1,15 +1,39 @@
 import mongoose from "mongoose";
 
-const initialBoardState = () => [
-  ["R", "N", "B", "Q", "K", "B", "N", "R"],
-  Array(8).fill("P"),
-  Array(8).fill(""),
-  Array(8).fill(""),
-  Array(8).fill(""),
-  Array(8).fill(""),
-  Array(8).fill("p"),
-  ["r", "n", "b", "q", "k", "b", "n", "r"]
-];
+const initialBoardState = {
+  a1: { color: "white", type: "rook" },
+  b1: { color: "white", type: "knight" },
+  c1: { color: "white", type: "bishop" },
+  d1: { color: "white", type: "queen" },
+  e1: { color: "white", type: "king" },
+  f1: { color: "white", type: "bishop" },
+  g1: { color: "white", type: "knight" },
+  h1: { color: "white", type: "rook" },
+  a2: { color: "white", type: "pawn" },
+  b2: { color: "white", type: "pawn" },
+  c2: { color: "white", type: "pawn" },
+  d2: { color: "white", type: "pawn" },
+  e2: { color: "white", type: "pawn" },
+  f2: { color: "white", type: "pawn" },
+  g2: { color: "white", type: "pawn" },
+  h2: { color: "white", type: "pawn" },
+  a7: { color: "black", type: "pawn" },
+  b7: { color: "black", type: "pawn" },
+  c7: { color: "black", type: "pawn" },
+  d7: { color: "black", type: "pawn" },
+  e7: { color: "black", type: "pawn" },
+  f7: { color: "black", type: "pawn" },
+  g7: { color: "black", type: "pawn" },
+  h7: { color: "black", type: "pawn" },
+  a8: { color: "black", type: "rook" },
+  b8: { color: "black", type: "knight" },
+  c8: { color: "black", type: "bishop" },
+  d8: { color: "black", type: "queen" },
+  e8: { color: "black", type: "king" },
+  f8: { color: "black", type: "bishop" },
+  g8: { color: "black", type: "knight" },
+  h8: { color: "black", type: "rook" }
+};
 
 const GameSchema = new mongoose.Schema(
   {
@@ -25,11 +49,139 @@ const GameSchema = new mongoose.Schema(
     },
 
     boardState: {
-      type: Array,
+      type: {
+        a1: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        b1: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        c1: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        d1: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        e1: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        f1: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        g1: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        h1: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        a2: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        b2: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        c2: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        d2: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        e2: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        f2: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        g2: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        h2: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        a7: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        b7: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        c7: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        d7: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        e7: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        f7: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        g7: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        h7: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        a8: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        b8: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        c8: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        d8: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        e8: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        f8: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        g8: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        },
+        h8: {
+          type: { type: String, required: false },
+          color: { type: String, required: false }
+        }
+      },
       required: true,
-      default: initialBoardState()
+      default: initialBoardState
     },
-
     currentPlayer: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
